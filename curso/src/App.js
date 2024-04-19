f918195339cceebed46f370bbc178cc8832a6974
Hudson Falcão Silva <falcaoh@gmail.com>
2024-04-19 12:30:31 -0300

    Aula 9 Eventos de compontentes

--- curso/src/App.js
+++ curso/src/App.js
@@ -1,26 +1,13 @@
 import './App.css'
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import Frase from './components/Frase'
-import List from './components/List'
-
+import Evento from './components/Evento';
+import Form from './components/Form';
 function App() {
-  const nome = "Maria"
   return (
     <div className="App">
-      <h1>Testanto CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="Matheus" />
-      <SayMyName nome="João" />
-      <SayMyName nome={nome} />
-      <Pessoa 
-        nome="Rodrigo" 
-        idade="43" 
-        profissao="Analista" 
-        foto="https://via.placeholder.com/150" 
-      />
-      <List />
+      <h1>Testanto Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      <Form />
     </div>
   )
 }
