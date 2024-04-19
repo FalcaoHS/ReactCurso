a1c73722fdba426695ad2be5d3c71db6aa1aba09
Hudson Falcão Silva <falcaoh@gmail.com>
2024-04-19 11:25:57 -0300

    Aulta 05

--- curso/src/App.js
+++ curso/src/App.js
@@ -1,21 +1,15 @@
-import './App.css';
+import './App.css'
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-
-  const name = 'Hudson'
-  const newname = name.toUpperCase()
-  function sum(a,b){
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
-
+  const nome = "Maria"
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {newname}</p>
-      <p>Soma: {sum(1,2)}</p>
-      <img scr={url} alt="Minha imagem" />
+      <SayMyName nome="Matheus" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Rodrigo" idade="43" profissao="Analista" foto="https://via.placeholder.com/150" />
     </div>
   );
 }
