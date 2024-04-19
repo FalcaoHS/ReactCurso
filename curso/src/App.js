f65fa316a7c77a682ddb1374799d549afb75a446
Hudson Falcão Silva <falcaoh@gmail.com>
2024-04-19 12:07:23 -0300

    Aula 8 Props Avançado - Tipação de propriedades de Componentes.

--- curso/src/App.js
+++ curso/src/App.js
@@ -1,17 +1,28 @@
 import './App.css'
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
   const nome = "Maria"
   return (
     <div className="App">
+      <h1>Testanto CSS</h1>
+      <Frase />
+      <Frase />
       <SayMyName nome="Matheus" />
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
-      <Pessoa nome="Rodrigo" idade="43" profissao="Analista" foto="https://via.placeholder.com/150" />
+      <Pessoa 
+        nome="Rodrigo" 
+        idade="43" 
+        profissao="Analista" 
+        foto="https://via.placeholder.com/150" 
+      />
+      <List />
     </div>
-  );
+  )
 }
 
 export default App;
