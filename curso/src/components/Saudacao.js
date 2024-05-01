345afd4926122cb75b64a7b9b23081e7c749ad46
Hudson Falcão Silva <falcaoh@gmail.com>
2024-05-01 16:19:42 -0300

    Curso React: State Lift - #14

---
+++ curso/src/components/Saudacao.js
@@ -0,0 +1,9 @@
+function Saudacao({nome}){
+    function gerarSaudacao(algumnome){
+       return `Olá, ${algumnome}, tudo bem?`
+    }
+
+    return<>{nome && <p>{gerarSaudacao(nome)}</p>}</>
+}
+
+export default Saudacao
